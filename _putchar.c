4f714d1710224cd2f2e53d6c8ae635f9698e6c30
Metin100 <9455@holbertonstudents.com>
2024-07-23 16:40:53 +0400

    Fixing Betty Problems

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,11 @@
 #include "unistd.h"
-
+/**
+ * _putchar- function
+ * @c: parameter
+ * Return: 0
+ */
 int _putchar(char c)
 {
-        write(1, &c, 1);
-        return (0);
+	write(1, &c, 1);
+	return (0);
 }
--- printf.c
+++ printf.c
@@ -1,56 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * _printf - function
+ * @format: pointer
+ * Return: length
+ */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int i = 0, j = 0, len = 0;
-        char *tmp;
-        va_start(args,format);
-        if (format[i])
-        {
-                while (format[i])
-                {
-                        if (format[i] == '%' && format[i + 1] == 'c')
-                        {
-                                _putchar(va_arg(args, int));
-                                len++;
-                                i = i + 2;
-                        }
-                        else if (format[i] == '%' && format[i + 1] == 's')
-                        {
-                                tmp = va_arg(args, char*);
-                                j = 0;
-                                if (tmp == NULL)
-                                {
-                                        tmp = "(null)";
-                                }
-                                while (tmp[j])
-                                {
-                                        _putchar(tmp[j]);
-                                        j++;
-                                }
-                                len = len + j;
-                                i = i + 2;
-                        }
-                        else if (format[i] == '%' && format[i + 1] == '%')
-                        {
-                                _putchar('%');
-                                len++;
-                                i = i + 2;
-                        }
-			else if (format[i] == '%' && format[i + 1] == '\0')
-			{
+	va_list args;
+	int i = 0, j = 0, len = 0;
+	char *tmp;
+
+	va_start(args, format);
+	if (format[i])
+	{
+		while (format[i])
+		{
+			if (format[i] == '%' && format[i + 1] == '\0')
 				return (-1);
+			else if (format[i] == '%' && format[i + 1] == 'c')
+				_putchar(va_arg(args, int)), len++, i = i + 2;
+			else if (format[i] == '%' && format[i + 1] == 's')
+			{
+				tmp = va_arg(args, char*);
+				j = 0;
+				if (tmp == NULL)
+					tmp = "(null)";
+				while (tmp[j])
+					_putchar(tmp[j]), j++;
+				len = len + j, i = i + 2;
 			}
+			else if (format[i] == '%' && format[i + 1] == '%')
+				_putchar('%'), len++, i = i + 2;
 			else
-                        {
-                                _putchar(format[i]);
-                                i++;
-                                len++;
-                        }
-                }
-        }
-        va_end(args);
-        return (len);
+				_putchar(format[i]), i++, len++;
+		}
+	}
+	va_end(args);
+	return (len);
 }
