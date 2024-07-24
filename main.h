cfa1e410c014ed282bd829a8671b78e625ac5de8
Metin100 <9455@holbertonstudents.com>
2024-07-24 16:27:38 +0400

    adding our new functions to header file

--- main.h
+++ main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_int(va_list arg);
 #endif
